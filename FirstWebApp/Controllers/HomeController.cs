ffb79878d38c930e908b02014050654978e92971
Vusalmb <vusalmb@code.edu.az>
2021-12-16 12:57:26 +0400

    Добавьте файлы проекта.

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            //return Content("Index P223");
+            ViewResult view = new ViewResult();
+            view.ViewName = "Index";
+            return view;
+        }
+
+        public ActionResult About()
+        {
+            //return Content("About P223");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //return Content("Contact P223");
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            //return Content("Contact P223");
+            return View();
+        }
+    }
+}
